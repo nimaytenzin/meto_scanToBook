9de578e13b6abb2342ea657259a783aa0b95d8a5
kinley wangyel <kwangyel@gmail.com>
2021-12-22 04:41:33 +0600

    auth header

--- src/services/routeServices.js
+++ src/services/routeServices.js
@@ -36,7 +36,7 @@ export async function getAllRoutePaths(){
 }
 
 export async function createNewRoutePath(data){
-    return await axios.post(`${devUrl}/route-path`,data)
+    return await axios.post(`${devUrl}/route-path`,data,{headers:authHeader})
 }
 
 export async function editRoute(id,data){
